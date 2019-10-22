dccd43f219fe60292c50d73e673c166b264ef837
Thiago Giannini Ramos <26007051@ACADEMICO>
2019-10-22 20:03:27 -0300

    Primeiro exemplo utilizando Primefaces

---
+++ src/control/PessoaBean.java
@@ -0,0 +1,56 @@
+package control;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import dao.PessoaDAO;
+import model.Pessoa;
+import util.FabricaConexao;
+
+@ManagedBean
+@ViewScoped
+public class PessoaBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Pessoa pessoa;
+	private List<Pessoa> pessoas;
+	
+	public PessoaBean() {
+		this.pessoa = new Pessoa();
+		this.pessoas = new ArrayList<Pessoa>();
+	}
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	@PostConstruct
+	public void init() {
+		try {
+			
+			FabricaConexao fabrica = new FabricaConexao();
+			Connection conexao = fabrica.fazerConexao();
+			
+			PessoaDAO dao = new PessoaDAO(conexao);
+			
+			this.pessoas = dao.listarTodos();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
